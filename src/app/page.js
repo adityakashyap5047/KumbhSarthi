b88f0e6619bfb184abdb45bc441971c946eacfa9
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-15 15:26:21 +0530

    adding the components using the magicui

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import NumberTicker from "@/components/magicui/number-ticker";
 
 function page() {
   return (
     <div>
       <p>Home Page</p>
       <Button>Click me</Button>
+      <p className="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-sky-600 dark:text-white ">
+        <NumberTicker value={50} />
+      </p>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
